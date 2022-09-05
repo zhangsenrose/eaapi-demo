61aa9d67162baaa27f81a67ef6e6fcab209d328f
zhangsenrose@163.com <zhangsenrose@163.com>
2022-09-06 00:25:28 +0800

    add crawler

---
+++ src/main/java/com/example/esapidemo/crawler/BasicCrawlerController.java
@@ -0,0 +1,43 @@
+package com.example.esapidemo.crawler;
+
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+
+public class BasicCrawlerController {
+
+    public static void main(String[] args) throws Exception {
+
+        String crawlStorageFolder = "/data/crawl/root";
+        int numberOfCrawlers = 7;
+
+        CrawlConfig config = new CrawlConfig();
+        config.setCrawlStorageFolder(crawlStorageFolder);
+
+        // Instantiate the controller for this crawl.
+        PageFetcher pageFetcher = new PageFetcher(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+        CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+
+        // For each crawl, you need to add some seed urls. These are the first
+        // URLs that are fetched and then the crawler starts following links
+        // which are found in these pages
+        controller.addSeed("https://www.ics.uci.edu/~lopes/");
+        controller.addSeed("https://www.ics.uci.edu/~welling/");
+        controller.addSeed("https://www.ics.uci.edu/");
+
+        // The factory which creates instances of crawlers.
+        CrawlController.WebCrawlerFactory<MyCrawler> factory = MyCrawler::new;
+
+        // Start the crawl. This is a blocking operation, meaning that your code
+        // will reach the line after this only when crawling is finished.
+        controller.start(factory, numberOfCrawlers);
+    }
+
+
+
+
+}
