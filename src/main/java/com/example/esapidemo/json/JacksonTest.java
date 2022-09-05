f17dfb42c28d1aa6c7ffbdd0a381650829e1dee9
zhangsenrose@163.com <zhangsenrose@163.com>
2022-09-06 00:12:42 +0800

    cm

--- src/main/java/com/example/esapidemo/json/JacksonTest.java
+++ src/main/java/com/example/esapidemo/json/JacksonTest.java
@@ -1,2 +1,22 @@
-package com.example.esapidemo.json;public class JackjsonTest {
+package com.example.esapidemo.json;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class JacksonTest {
+
+
+    public static void main(String[] args) throws JsonProcessingException {
+
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "zhangsenwei");
+        map.put("age", 40);
+        String valueAsString = mapper.writeValueAsString(map);
+        System.out.println(valueAsString);
+
+    }
 }
